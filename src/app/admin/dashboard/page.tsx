9aecc87776243e9b7499c231dc351a733a0b5961
Salvador De la Tijera <salvagerar@gmail.com>
2026-02-20 18:34:09 -0600

    creaciond e logica del chat admin  usuarios

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -4,19 +4,19 @@ import { useState } from "react";
 import AdminLayout from "@/src/components/admin/AdminLayout";
 import UsersSection from "@/src/components/admin/UsersSection";
 import ProductRequestsSection from "@/src/components/admin/ProductRequestsSection";
+import AdminChatsSection from "@/src/components/admin/AdminChatsSection";
 
 export default function AdminDashboard() {
-const [section, setSection] = useState<"users" | "products">("users");
+  const [section, setSection] = useState<"users" | "products" | "chats">(
+    "users",
+  );
 
 
   return (
     <AdminLayout section={section} setSection={setSection}>
-      {section === "users" ? (
-        <UsersSection />
-      ) : (
-        <ProductRequestsSection />
-      )}
+      {section === "users" && <UsersSection />}
+      {section === "products" && <ProductRequestsSection />}
+      {section === "chats" && <AdminChatsSection />}
     </AdminLayout>
   );
 }
-
